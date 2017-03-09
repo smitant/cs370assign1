d2ee1812bbb9e2fbf2a7317cdf6cbf7a3c699dbe
smitant <smitant563@gmail.com>
2017-03-08 22:21:39 -0600

    IO changes

--- cs370proj1V2/cs370proj1V2.cpp
+++ cs370proj1V2/cs370proj1V2.cpp
@@ -8,10 +8,10 @@ struct registerRW
 {
 	int read;
 	int write;
-	registerRW() {
-		read = 0;
-		write = 0;
-	}
+	registerRW() :
+		read(0),
+		write(0) {};
+	
 };
 string printReg(registerRW);
 
@@ -20,86 +20,71 @@ int main()
 {
 	//output variables
 	int insts = 0, r_type = 0, i_type = 0, j_type = 0, fwd_taken = 0, bkw_taken = 0, not_taken = 0, loads = 0, stores = 0;
-	//is this how you define a new array? c++ is odd...
+	//is this how you define a new array? also from what i understand since i have my constructor are there already
+	//initilized values in all of the slotted array spots?
 	registerRW registers[31];
-	/*for (size_t i = 0; i < 31; i++)
-	{
-		//i am unsure if the use of tmp is correct here? ask prof on tuesday...
-		registerRW * tmp = new registerRW();
-		registers[i] = tmp;
-	}*/
-	//otherwise do it by hand... it makes me cringe
-	registerRW * reg1 = new registerRW();
-	registerRW * reg2 = new registerRW();
-	registerRW * reg3 = new registerRW();
-	registerRW * reg4 = new registerRW();
-	registerRW * reg5 = new registerRW();
-	registerRW * reg6 = new registerRW();
-	registerRW * reg7 = new registerRW();
-	registerRW * reg8 = new registerRW();
-	registerRW * reg9 = new registerRW();
-	registerRW * reg10 = new registerRW();
-	registerRW * reg11 = new registerRW();
-	registerRW * reg12 = new registerRW();
-	registerRW * reg13 = new registerRW();
-	registerRW * reg14 = new registerRW();
-	registerRW * reg15 = new registerRW();
-	registerRW * reg16 = new registerRW();
-	registerRW * reg17 = new registerRW();
-	registerRW * reg18 = new registerRW();
-	registerRW * reg19 = new registerRW();
-	registerRW * reg20 = new registerRW();
-	registerRW * reg21 = new registerRW();
-	registerRW * reg22 = new registerRW();
-	registerRW * reg23 = new registerRW();
-	registerRW * reg24 = new registerRW();
-	registerRW * reg25 = new registerRW();
-	registerRW * reg26 = new registerRW();
-	registerRW * reg27 = new registerRW();
-	registerRW * reg28 = new registerRW();
-	registerRW * reg29 = new registerRW();
-	registerRW * reg30 = new registerRW();
-	registerRW * reg31 = new registerRW();
-
 	
 	//needed variables for I/O
 	ofstream outFile;
-	ifstream inputFile;
+	FILE * inputFile;
+	//ifstream inputFile1;
 	string stringLine;
-	unsigned pcLine, instrucLine;
+	unsigned int pcLine, instrucLine;
 	int instructionIndex = 0;
+	//inputFile1.open("trace.txt");
+	
 
-	inputFile.open("trace.txt");
+	fopen_s(&inputFile,"trace.txt", "r");
 	outFile.open("statistics.txt");
-	if (inputFile.is_open())
+	//outFile << registers[0].read;
+	//outFile << registers[0].write;
+
+	/*
+	while (inputFile1 >> pcLine >> instrucLine) {
+		outFile << pcLine << " " << instrucLine;
+	}
+	inputFile1.close();
+	*/
+	if (inputFile != NULL)
 	{
-		while (inputFile >> hex >> pcLine >> hex >> instrucLine)
+		while (fscanf_s(inputFile, "%x %x", &pcLine, &instrucLine) == 2)
 		{
+			insts++;
+			
+			switch ((instrucLine >> 26))
+			{
+			case 0x00: r_type++;
+
+			default:
+				break;
+			}
 			//do all computations and incrementing of variables here
-			outFile << pcLine << " " << instrucLine;
+			outFile << pcLine << " " << instrucLine << endl;
+			
+
+
 		}
-		inputFile.close();
+		//inputFile.close();
 	}
 	else {
 		cout << "Unable to open file";
 		return -1;
 	}
-
-
+	
 
 	
-	outFile << "insts:";
-	outFile << "r-type:";
-	outFile << "i-type:";
-	outFile << "j-type:";
-	outFile << "fwd-taken:";
-	outFile << "bkw-taken:";
-	outFile << "not-taken:";
-	outFile << "loads:";
-	outFile << "stores:";
+	outFile << "insts: " << insts << endl;
+	outFile << "r-type: " << r_type << endl;
+	outFile << "i-type:\n";
+	outFile << "j-type:\n";
+	outFile << "fwd-taken:\n";
+	outFile << "bkw-taken:\n";
+	outFile << "not-taken:\n";
+	outFile << "loads:\n";
+	outFile << "stores:\n";
 	for (size_t i = 0; i < 31; i++)
 	{
-		//outFile << "reg-" << i << ":" << printReg();
+		//outFile << "reg-" << i << ":" << printReg() << "\n";
 	}
 	outFile.close();
     return 0;
